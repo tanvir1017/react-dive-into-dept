ea50e681d89e9f2d0e3e928898bcf2cc3b3eea1f
tanvir1017 <tanvir.hossaindev1@gmail.com>
2023-02-27 22:32:59 +0600

    feat: 🎸 Initializing the react-dive-into-depth project
    Initializing the react-dive-into-depth project

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import "./App.css";
+import reactLogo from "./assets/react.svg";
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src="/vite.svg" className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://reactjs.org" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <div>
+        <h1>React Dive Into Depth 🚀🚀</h1>
+        <p>
+          This repository is create to note all about React conceptual things.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
